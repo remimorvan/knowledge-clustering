b83dfe543b75aedf72ba05b9e4170e623cdd143f
remimorvan <m.remimorvan@gmail.com>
2022-02-24 23:35:26 +0100

    new distance

--- distance.py
+++ distance.py
@@ -15,29 +15,54 @@ def commonFactor(s1, s2):
     return maxCommonFactor
 
 def normaliseNotion(notion):
-    # Returns the (substring) of a notion that should be considered when computing the distance between two notions
-    # Obtained by removing any math and command
-    notionSub = notion
-    while '$' in notionSub:
-        sp = notionSub.split("$",2)
+    # Returns the substring of a notion that should be considered when computing the distance between two notions
+    # Obtained by removing math, commands, spaces, dashes, and by writing the notion in lowercase.
+    notion_norm = notion
+    while '$' in notion_norm:
+        sp = notion_norm.split("$",2)
         if len(sp) <= 1:
             break
-        notionSub = sp[0] + sp[2]
-    while '\\' in notionSub and ' ' in notionSub:
-        sp = notionSub.split("\\", 1)
+        notion_norm = sp[0] + sp[2]
+    while '\\' in notion_norm and ' ' in notion_norm:
+        sp = notion_norm.split("\\", 1)
         sp2 = (sp[1]).split(" ", 1)
         if len(sp2) == 0:
             break
-        notionSub = sp[0] + sp2[1]
-    while notionSub[0] in [' ', '-']:
-        notionSub = notionSub[1:]
-    while notionSub[-1] in [' ', '-']:
-        notionSub = notionSub[:-1]
-    return notionSub
+        notion_norm = sp[0] + sp2[1]
+    while notion_norm[0] in [' ', '-']:
+        notion_norm = notion_norm[1:]
+    while notion_norm[-1] in [' ', '-']:
+        notion_norm = notion_norm[:-1]
+    notion_norm = notion_norm.replace(" ", "")
+    notion_norm = notion_norm.replace("-", "")
+    return notion_norm.lower()
+
+def extractScope(notion):
+    # Given a notion of the form "knowledge@scope" or "knowledge",
+    # returns a pair consisting of the knowledge and the (possibly empty) scope.
+    if '@' in notion:
+        s = notion.split('@',1)
+        return s[0], s[1]
+    else:
+        return notion, ""
+
 
 def distance(notion1, notion2):
     # measures the distance between two strings
-    notionMod1 = normaliseNotion(notion1)
-    notionMod2 = normaliseNotion(notion2)
-    n = (len(notionMod1)+len(notionMod2))/2
-    return commonFactor(notionMod1, notionMod2)/n
+    notion_norm1 = normaliseNotion(notion1)
+    knowledge1, scope1 = extractScope(notion_norm1)
+    notion_norm2 = normaliseNotion(notion2)
+    knowledge2, scope2 = extractScope(notion_norm2)
+    if scope1 == scope2:
+        n = (len(knowledge1)+len(knowledge2))/2
+        return 1 - commonFactor(knowledge1, knowledge2)/n
+    if scope1 != "" and scope2 != "" and scope1 != scope2:
+        return 1
+    else: 
+        if scope2 != "":
+            knowledge1, scope1, knowledge2, scope2 = knowledge2, scope2, knowledge1, scope1
+        # Now scope2 is empty
+        n = (len(knowledge1+scope1)+len(knowledge2))/2
+        cf1 = commonFactor(scope1+knowledge1, knowledge2)
+        cf2 = commonFactor(knowledge1+scope1, knowledge2)
+        return 1 - max(cf1, cf2)/n
