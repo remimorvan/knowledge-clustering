f22333e2423a74efff5446eb0ec0abffa5b023e9
remimorvan <m.remimorvan@gmail.com>
2022-03-04 23:02:28 +0100

    only print unknown knowledges

--- parse_asp.py
+++ parse_asp.py
@@ -18,17 +18,22 @@ def getFirstPredicate(solution):
     return(solution, "")
 
 def parsePredicate(str):
-    # Takes a predicate as returned by clingo (e.g. 'belongsTo("word@ord",0)')
+    # Takes a predicate as returned by clingo (e.g. 'belongsToUnkown("word@ord",0)')
     # and returns the knowledge ("word@ord"), and the id of the bag it belongs to (here, it is 0).
     lst = str.split('(')
-    predicate = str[0]
-    if predicate == "belongsTo":
+    predicate = lst[0]
+    if predicate == "belongsToUnkown":
         lst = lst[1].split(')')
         lst = lst[0].split(',')
         return (lst[0], lst[1])
+    else:
+        raise UnknownPredicate(str[0])
 
 with open("solution.txt") as f:
     lines = f.read().splitlines()
     solution = lines[0]
-    print(getAllPredicates(solution))
-    raise UnknownPredicate("abc")
+    while solution != "":
+        pred, solution = getFirstPredicate(solution)
+        kl, bag_str = parsePredicate(pred)
+        bag_id = int(bag_str)
+        print("%s in bag %i" % (kl, bag_id))
