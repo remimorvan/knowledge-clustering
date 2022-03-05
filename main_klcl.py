2058709e9bfa5dd6cde92af57ee7c36c959874d0
remimorvan <m.remimorvan@gmail.com>
2022-03-05 10:26:13 +0100

    missing points when calling kltex functions

--- main_klcl.py
+++ main_klcl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import diagnose as diag
+import kltex
 
 import sys, getopt
 
@@ -8,23 +9,23 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv,"hn:d:",["notion=","diagnose"])
     except getopt.GetoptError:
-        print("main-klcl.py -n <file_notion> -d <file_diagnose>")
+        print("main_klcl.py -n <file_notion> -d <file_diagnose>")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("main-klcl.py -n <file_notion> -d <file_diagnose>")
+            print("main_klcl.py -n <file_notion> -d <file_diagnose>")
             sys.exit()
         elif opt in ("-n", "--notion"):
             notion_file = arg
         elif opt in ("-d", "--diagnose"):
             diagnose_file = arg
     with open(notion_file) as nf:
-        document, known_knowledges = kltexparse(nf)
+        document, known_knowledges = kltex.parse(nf)
         nf.close()
     undefined_knowledges = diag.parse(diagnose_file)
     with open(notion_file, "w") as nf: # otherwise, use sys.stdout as nf
         output = [[k] for k in undefined_knowledges]
-        kltexwriteDocument(nf, document, [], output)
+        kltex.writeDocument(nf, document, [], output)
         nf.close()
 
 if __name__ == "__main__":
