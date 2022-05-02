5469882fe60df361c4767ff028c4693e90fd22c3
Aliaume LOPEZ <aliaume.lopez@lsv.fr>
2022-05-02 11:46:04 +0200

    first step in automatic detection of knowledges

---
+++ knowledge_clustering/autofinder.py
@@ -0,0 +1,18 @@
+import os
+
+
+def autofinder():
+    """Automatically finds
+    suitable .diagnose and .notion
+    files in the current working directory
+    and yields them
+    """
+
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if os.path.basename(file).endswith(".diagnose"):
+                yield ("notion", file)
+            elif os.path.basename(file).endswith(".knowledge") or os.path.basename(
+                file
+            ).endswith(".kw"):
+                yield ("knowledge", file)
