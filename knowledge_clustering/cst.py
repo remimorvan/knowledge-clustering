0b80922847eccb2f42b6044a45efcb37260c4b2a
Rémi Morvan <remi@morvan.xyz>
2023-12-28 18:56:29 +0100

    migration pkg_resources -> importlib

--- knowledge_clustering/cst.py
+++ knowledge_clustering/cst.py
@@ -4,17 +4,19 @@ Constants used throughout knowledge-clustering.
 
 from __future__ import annotations  # Support of `|` for type union in Python 3.9
 
-import pkg_resources
+from importlib import resources
 
 ALPHA = 0
 
 CONFIG_FILENAME: dict[str, str] = {"en": "english.ini", "fr": "french.ini"}
-CONFIG_DIR: str = pkg_resources.resource_filename("knowledge_clustering", "data")
+ref = resources.files('knowledge_clustering') / 'data'
+with resources.as_file(ref) as path:
+    CONFIG_DIR: str = path
 CONFIG_FILE: dict[str, str] = dict()
 for lan, filename in CONFIG_FILENAME.items():
-    CONFIG_FILE[lan] = pkg_resources.resource_filename(
-        "knowledge_clustering", f"data/{filename}"
-    )
+    ref_file =  resources.files('knowledge_clustering') / f"data/{filename}"
+    with resources.as_file(ref_file) as path_file:
+        CONFIG_FILE[lan] = path_file
 NLTK_LANG: dict[str, str] = {"en": "english", "fr": "french"}
 
 INTRO_DELIMITERS: list[tuple[str, str]] = [
@@ -89,6 +91,6 @@ BEGIN_GREEN: str = "\033[32m"
 END_EMPH: str = "\033[0m"
 
 TIMEOUT_REQUEST: float = (
-    0.25  # Timeout to resquest the latest version
+    0.2  # Timeout to resquest the latest version
     # of knowledge-clustering (in seconds)
 )
