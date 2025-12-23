326a2617b6596645dd7b783fa1da35a6ab1f8940
Rémi Morvan <remi@morvan.xyz>
2025-12-23 21:38:28 +0100

    interface

--- knowledge_clustering/add_quotes.py
+++ knowledge_clustering/add_quotes.py
@@ -3,14 +3,103 @@ Add missing quotes around knowledges occuring in a TeX document.
 """
 
 from __future__ import annotations  # Support of `|` for type union in Python 3.9
+from enum import Enum, Flag, auto
 
 import re  # Regular expressions
-from typing import NamedTuple, TextIO
+from typing import NamedTuple, TextIO, Any
+from collections.abc import Generator, Hashable, Callable
 import sys
 
 from knowledge_clustering.knowledges import KnowledgesList, remove_redundant_files
 from knowledge_clustering.tex_document import TexDocument
 from knowledge_clustering import file_updater, misc, cst
+
+state = Hashable
+
+
+class Action:
+    abbrev: str
+    fullname: str
+    __is_feasible: Callable[[Interface], bool]
+    __execute: Callable[[Interface], Interface]
+
+    def __init__(
+        self,
+        abbrev: str,
+        fullname: str,
+        is_feasible: Callable[[Interface], bool],
+        execute: Callable[[Interface], Interface],
+    ) -> None:
+        self.abbrev = abbrev
+        self.fullname = fullname
+        self.__is_feasible = is_feasible
+        self.__execute = execute
+
+    def get_abbrev(self) -> str:
+        return self.abbrev
+
+    def __str__(self) -> str:
+        return misc.emph(self.abbrev) + " (" + self.fullname + ")"
+
+    def is_feasible(self, interface: Interface) -> bool:
+        return self.__is_feasible(interface)
+
+    def execute(self, interface: Interface) -> Interface:
+        return self.__execute(interface)
+
+
+class Interface:
+    states: set[state]
+    current_state: state
+    final_states: set[state]
+    filename: str
+    actions: list[Action]
+    registers: dict[Hashable, Any]
+
+    def __init__(
+        self,
+        states: list[state],
+        initial_state: state,
+        final_states: list[state],
+        actions: list[Action],
+    ) -> None:
+        self.states = set(states)
+        self.actions = actions
+        self.current_state = initial_state
+        self.final_states = set(final_states)
+        self.registers = {}
+
+    def get_action_from_abbrev(self, abbrev: str) -> Action:
+        for act in self.actions:
+            if act.get_abbrev() == abbrev:
+                return act
+        raise KeyError(abbrev)
+
+    def get_available_actions(self) -> list[Action]:
+        """Returns actions available in the current state."""
+        return [act for act in self.actions if act.is_feasible(self)]
+
+    def __print_context(self) -> None:
+        print(f"Current state: {self.current_state}")
+
+    def __print_and_ask_actions(self) -> Action:
+        act: Action | None = None
+        while not act:
+            for act in self.get_available_actions():
+                print(act, end=" ")
+            try:
+                inp = input()
+                act = self.get_action_from_abbrev(inp)
+            except KeyError:
+                print(f"'{inp}' does not correspond to a valid action.")
+                act = None
+        return act
+
+    def interact(self) -> None:
+        while not self.current_state in self.final_states:
+            self.__print_context()
+            act = self.__print_and_ask_actions()
+            act.execute(self)
 
 
 class NewKL(NamedTuple):
